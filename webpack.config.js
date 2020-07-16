cd9aeb557942f021ed08e7391c7f1a70dd301444
Omar M Juarez <omarmju906p@hotmail.com>
2020-07-15 20:00:26 -0500

    Se configura Babel y Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebPlackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node-modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader"
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPlackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
